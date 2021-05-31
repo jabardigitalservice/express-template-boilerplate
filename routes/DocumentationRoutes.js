9b1205969e8de5f3f8b4aff5502d4e95af31013d
Firman Abdul Hakim <fimzhakim@gmail.com>
2021-05-31 15:02:56 +0700

    Initial commit

---
+++ routes/DocumentationRoutes.js
@@ -0,0 +1,24 @@
+const swaggerUi = require('swagger-ui-express')
+const express = require('express')
+const { API_PATH } = require('../helpers/constant')
+const { customResponse } = require('../helpers/exceptions')
+const { index } = require('../static')
+
+const router = express.Router()
+
+router.use(`${API_PATH}/documentation`, swaggerUi.serve)
+router.get(`${API_PATH}/documentation`, swaggerUi.setup(index))
+router.get('/', (req, res) => {
+  customResponse(res, 200, 'Api running', {
+    welcome: 'Welcome to Boilerplate API.',
+    hostname: require('os').hostname(),
+    uptime: require('os').uptime(),
+    loadavg: require('os').loadavg(),
+    totalmem: require('os').totalmem(),
+    version: require('os').version(),
+    platform: require('os').platform(),
+    release: require('os').release(),
+    documentation: `http://${req.get('host')}/api/v1/documentation`,
+  })
+})
+module.exports = router
